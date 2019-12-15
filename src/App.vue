529ce5c78ab29ecaaa4e4d58a10c8b487f4382ad
Sam Maaß <sam@maass.co>
2019-12-15 12:56:31 +0100

    initial implementation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,89 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="grid">
+      <div
+        class="hour"
+        v-for="hour in hours"
+        :key="hour"
+        :style="{ gridRowStart: 1 + hour - startHour }"
+      >
+        {{ hour }}
+      </div>
+      <div
+        class="delivery-slot"
+        v-for="slot in deliverySlots"
+        :key="slot.start + '-' + slot.end"
+        :style="{
+          gridRowStart: 1 + slot.start - startHour,
+          gridRowEnd: 1 + slot.end - startHour
+        }"
+      >
+        {{ slot.start }}:00 - {{ slot.end }}:00
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  components: {},
+  data() {
+    return {
+      startHour: 8,
+      endHour: 22,
+      deliverySlots: [
+        { start: 8, end: 10 },
+        { start: 8, end: 20 },
+        { start: 10, end: 12 },
+        { start: 10, end: 16 },
+        { start: 15, end: 17 }
+      ]
+    };
+  },
+  computed: {
+    hours() {
+      var list = [];
+      for (var i = this.startHour; i <= this.endHour; i++) {
+        list.push(i);
+      }
+      return list;
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.grid {
+  display: grid;
+  background: #eee;
+  grid-template-columns: 40px auto;
+}
+.hour {
+  grid-column-start: 1;
+  align-content: center;
+  justify-content: center;
+  height: 40px;
+  width: 40px;
+  background: #ddd;
+  display: grid;
+  align-content: center;
+}
+.delivery-slot {
+  background: #fff;
+  border: 2px solid #3d729e;
+  color: #3d729e;
+  border-radius: 8px;
+  margin: 4px;
+  display: grid;
+  align-content: center;
 }
 </style>
